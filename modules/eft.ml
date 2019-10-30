726bd5cda925ab4267d5e6d1594e9cec03e655bc
Laurent Thévenoux <lrnt@thvnx.com>
2019-10-30 23:27:43 +0100

    Add two_sum EFT

--- modules/eft.ml
+++ modules/eft.ml
@@ -13,23 +13,47 @@
    You should have received a copy of the GNU General Public License along with
    mlexn.  If not, see <http://www.gnu.org/licenses/>. *)
 
+
+(** Return value of an error-free transformation (EFT). *)
 type error_free_float = { high : float; low : float }
 
 
-let check_fp_class f =
+let check_fpclass f =
   match classify_float f with
     FP_subnormal -> failwith "subnormal number"
-  | FP_nan -> failwith "nan number"
-  | FP_infinite -> failwith "infinite number"
+  | FP_nan       -> failwith "nan number"
+  | FP_infinite  -> failwith "infinite number"
   | FP_zero
-    | FP_normal -> ()
+  | FP_normal    -> ()
 
+let fast_two_sum ?wa:(wa = false) op1 op2 =
+  let eft a b =
+    let sum = a +. b in
+    let err = b -. (sum -. a) in
+    { high = sum; low = err }
+  in
+  match wa with
+  | false -> eft op1 op2
+  | true  ->
+    check_fpclass op1;
+    check_fpclass op2;
+    if op1 < op2 then
+      failwith "fast_two_sum requires a >= b";
+    eft op1 op2
 
-let fast_two_sum a b =
-  if a < b then
-    failwith "fastTwoSum: a >= b assertion failed";
-  check_fp_class a;
-  check_fp_class b;
-  let sum = a +. b in
-  let err = b -. (sum -. a) in
-  { high = sum; low = err }
+let two_sum ?wa:(wa = false) op1 op2 =
+  let eft a b =
+    let sum = a +. b in
+    let t1 = sum -. a in
+    let t2 = sum -. t1 in
+    let e1 = b -. t1 in
+    let e2 = a -. t2 in
+    let err = e2 +. e1 in
+    { high = sum; low = err }
+  in
+  match wa with
+  | false -> eft op1 op2
+  | true  ->
+    check_fpclass op1;
+    check_fpclass op2;
+    eft op1 op2
