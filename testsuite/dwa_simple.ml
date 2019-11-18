7b312d12d32b490f24ce25c126a42361883f6b20
Laurent Thévenoux <lrnt@thvnx.com>
2019-11-18 21:25:41 +0100

    Add license headers

--- testsuite/dwa_simple.ml
+++ testsuite/dwa_simple.ml
@@ -1,3 +1,18 @@
+(* This file is part of mlexn.
+
+   mlexn is free software: you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+
+   mlexn is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General Public License along with
+   mlexn.  If not, see <http://www.gnu.org/licenses/>. *)
+
 let dwa x =
   Printf.printf "add_float      %s\n" (Dwa.to_string (Dwa.add_float (Dwa.of_float x) x));
   Printf.printf "sloppy_add     %s\n" (Dwa.to_string (Dwa.sloppy_add (Dwa.of_float x) (Dwa.of_float x)));
--- testsuite/twa_simple.ml
+++ testsuite/twa_simple.ml
@@ -1,3 +1,18 @@
+(* This file is part of mlexn.
+
+   mlexn is free software: you can redistribute it and/or modify it under the
+   terms of the GNU General Public License as published by the Free Software
+   Foundation, either version 3 of the License, or (at your option) any later
+   version.
+
+   mlexn is distributed in the hope that it will be useful, but WITHOUT ANY
+   WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+   FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+   details.
+
+   You should have received a copy of the GNU General Public License along with
+   mlexn.  If not, see <http://www.gnu.org/licenses/>. *)
+
 let twa x =
   Printf.printf "add          %s\n" (Twa.to_string (Twa.add (Twa.of_float x) (Twa.of_float x)));
   Printf.printf "sub          %s\n" (Twa.to_string (Twa.sub (Twa.of_float x) (Twa.of_float x)));
