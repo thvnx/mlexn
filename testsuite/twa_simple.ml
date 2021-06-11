e3a1b4e6508a27d57ac04bacbfa3429a946f71f1
Laurent Thévenoux <lrnt@thvnx.com>
2021-06-11 19:17:07 +0200

    Update twa_simple.ml
    Properly call mul_dwa_fast instead of mul_dwa.

--- testsuite/twa_simple.ml
+++ testsuite/twa_simple.ml
@@ -19,7 +19,7 @@ let twa x =
   Printf.printf "mul          %s\n" (Twa.to_string (Twa.mul (Twa.of_float x) (Twa.of_float x)));
   Printf.printf "mul_fast     %s\n" (Twa.to_string (Twa.mul_fast (Twa.of_float x) (Twa.of_float x)));
   Printf.printf "mul_dwa      %s\n" (Twa.to_string (Twa.mul_dwa (Twa.of_float x) (Dwa.of_float x)));
-  Printf.printf "mul_dwa_fast %s\n" (Twa.to_string (Twa.mul_dwa (Twa.of_float x) (Dwa.of_float x)));
+  Printf.printf "mul_dwa_fast %s\n" (Twa.to_string (Twa.mul_dwa_fast (Twa.of_float x) (Dwa.of_float x)));
   Printf.printf "reciprocal   %s\n" (Twa.to_string (Twa.reciprocal (Twa.of_float x)));
   Printf.printf "div          %s\n" (Twa.to_string (Twa.div (Twa.of_float x) (Twa.of_float x)));
   Printf.printf "sqrt         %s\n" (Twa.to_string (Twa.sqrt (Twa.of_float x)))
